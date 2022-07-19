db0d4fd1b58599363b1da9463749d0635400fd84
PJcodes <104080524+Pjcodes97@users.noreply.github.com>
2022-07-19 15:12:39 -0400

    Add timer, alert, and reset functions

--- reminder.py
+++ reminder.py
@@ -1,9 +1,8 @@
 from tkinter import *
-from tkinter import *
+from tkinter.ttk import *
 from time import *
 
 #Creates global variables to change and adjust
-countdown = False
 second = 0 
 minute = 0
 hour = 0
@@ -14,42 +13,72 @@ def clock_timer():
     clock.config(text = string)
     clock.after(1000, clock_timer)
 
+
 #Creates timer leading up the "alarm"
 def countdown():
     global second, minute, hour
     second += 1
     if second == 60:
-        minute = 1 
+        minute += 1 
         second = 0
         if minute == 60:
-            hour = 1
+            hour += 1
             minute = 0
     timer.config(text=f"{hour}:{minute}:{second}")
     timer.after(1000, countdown)
 
+
+#Creates Alert Label when the set time has passed
+def get_up_alert():
+    global second, minute, hour
+    string = "Get the fuck up"
+    if hour == 1 and minute == 30:
+        get_up_label.config(text=string)
+    get_up_label.after(1000, get_up_alert)
+
+
+#resets the timer to start over, does not STOP the timer, just resets to zero again
+def reset():
+    global second, minute, hour, cancel
+    root.after_cancel(countdown)
+    second = 0 
+    minute = 0 
+    hour = 0
+    get_up_label.config(text="") 
+
+
+#Creating Parent Window
 root = Tk()
 root.title("Stand Up and Stretch")
 root.geometry("500x500")
 
 #Creating a regular clock to keep track of time in general
-clock = Label()
+clock = Label(root)
 clock.pack()
 clock_timer()
 
-
-timer = Label(text=f"{hour}:{minute}:{second}")
+#Creates label for the countdown/ stopwatch
+timer = Label(root, text=f"{hour}:{minute}:{second}")
 timer.pack()
-countdown()
+
 
 #Creating start and stop buttons to begin timer for reminders
-start_button = Button(text="Start")
+start_button = Button(root, text="Start", command=countdown)
 start_button.pack()
 
-stop_button = Button(text="Stop")
-stop_button.pack()
+#Create button for reseting the timer back to zero
+reset_button = Button(root, text="Reset", command=reset)
+reset_button.pack()
+
+#Create label where "Get Up" Alarm/Warning will display
+get_up_label = Label(root)
+get_up_label.pack()
+get_up_alert()
 
 
 root.mainloop()
+
+
 
 
 
